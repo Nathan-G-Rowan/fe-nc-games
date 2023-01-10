f715281b239b279dffffeff11c3eb6d679d28b49
Nathan-G-Rowan <nathangrowan@gmail.com>
2023-01-10 10:06:24 +0000

    update css whilst plans were in review

--- src/elements/Reviews.jsx
+++ src/elements/Reviews.jsx
@@ -3,6 +3,7 @@ import { getReviews } from "../utils/api";
 import { ReviewCard } from "./ReviewCard";
 
 export const Reviews = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
   const [reviews, setReviews] = useState([]);
   const [organise, setOrganise] = useState({
     category: undefined,
@@ -12,23 +13,30 @@ export const Reviews = () => {
 
   useEffect(() => {
     getReviews(organise.category, organise.sort_by, organise.orderDesc).then(
-      (reviews) => setReviews(reviews)
+      (reviews) => {
+        setIsLoaded(true);
+        setReviews(reviews);
+      }
     );
   }, [organise]);
 
   return (
     <section className="ReviewList">
-      {reviews.length > 0 ? (
-        reviews.map((review) => (
-          <ReviewCard
-            title={review.title}
-            owner={review.owner}
-            votes={review.votes}
-            date={review.created_at.split("T")[0]}
-          />
-        ))
+      {isLoaded ? (
+        reviews.length > 0 ? (
+          reviews.map((review) => (
+            <ReviewCard
+              title={review.title}
+              owner={review.owner}
+              votes={review.votes}
+              date={review.created_at.split("T")[0]}
+            />
+          ))
+        ) : (
+          <p>nothing seems to be here...</p>
+        )
       ) : (
-        <p>nothing seems to be here...</p>
+        <p>Loading...</p>
       )}
     </section>
   );
